f565ea4079c3605868891706f4b600d366647c5e
Daniele Parisi <neb.inf@gmail.com>
2024-05-23 15:50:27 +0200

    Add configuration for native image build / push

--- src/main/java/com/danservice/fundmessenger/Application.java
+++ src/main/java/com/danservice/fundmessenger/Application.java
@@ -1,14 +1,16 @@
 package com.danservice.fundmessenger;
 
+import com.danservice.fundmessenger.adapter.outbound.kafka.streetorderack.v1.dto.KafkaStreetOrderAckDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @Slf4j
 @EnableKafka
 @SpringBootApplication
+@RegisterReflectionForBinding(KafkaStreetOrderAckDTO.class)
 public class Application {
 
     public static void main(String[] args) {
